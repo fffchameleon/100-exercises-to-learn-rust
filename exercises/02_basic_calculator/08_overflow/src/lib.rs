8c18a1bcbe7b3301ea48615246d6304c1053fef3
fffchamelon <clairechang312@gmail.com>
2024-06-27 11:27:32 +0800

    6/30 ~ 7/2 0210, finish 02

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -4,11 +4,12 @@
 //
 // For reasons that we'll explain later, the customization needs to be done in the `Cargo.toml`
 // at the root of the repository, not in the `Cargo.toml` of the exercise.
-
+use std::num::Wrapping;
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result : u32 = 1;
     for i in 1..=n {
-        result *= i;
+        result = result.wrapping_mul(i);
+        // result *= i;
     }
     result
 }
